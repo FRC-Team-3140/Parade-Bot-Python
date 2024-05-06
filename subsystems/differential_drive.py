c5167cbbbe585cd03a878bb5ba18e07d3f84a8d0
Grace Preston <143225518+Grass-Rat@users.noreply.github.com>
2024-05-06 12:08:05 -0400

    Added comments to differential_drive.py -GP
    For readability

--- subsystems/differential_drive.py
+++ subsystems/differential_drive.py
@@ -1,8 +1,6 @@
-#
 # Copyright (c) FIRST and other WPILib contributors.
 # Open Source Software; you can modify and/or share it under the terms of
 # the WPILib BSD license file in the root directory of this project.
-#
 
 import wpilib
 import wpilib.drive
@@ -15,7 +13,6 @@ import constants
 
 # create a motor controller translator to talonsrx
 class TalonMotorController(wpilib.interfaces.MotorController):
-
     def __init__(self, talon: phoenix5.TalonSRX, *args):
         super().__init__(*args)
         self.is_enabled = True
@@ -47,14 +44,14 @@ class TalonMotorController(wpilib.interfaces.MotorController):
         self.set(0)
 
 
-
-class DifferentalDrive(commands2.Subsystem):
+class DifferentialDrive(commands2.Subsystem):
     def __init__(self) -> None:
         """Creates a new DriveSubsystem"""
         super().__init__()
 
         self.frame_id = 0
 
+        # Motor CAN IDs
         kLeftSparkCANId = 9
         kLeftTalon1CANId = 4
         kLeftTalon2CANId = 6
@@ -65,36 +62,31 @@ class DifferentalDrive(commands2.Subsystem):
 
         kCurrentLimit = 40
 
+        # Initialize motors
         self.left_motor1 = CANSparkMax(kLeftSparkCANId, CANSparkMax.MotorType.kBrushless)
         self.left_talon2 = phoenix5.TalonSRX(kLeftTalon1CANId)
         self.left_talon3 = phoenix5.TalonSRX(kLeftTalon2CANId)
-
 
         self.right_motor1 = CANSparkMax(kRightSparkCANId, CANSparkMax.MotorType.kBrushless)
         self.right_talon2 = phoenix5.TalonSRX(kRightTalon1CANId)
         self.right_talon3 = phoenix5.TalonSRX(kRightTalon2CANId)
 
+        # Initialize Talon Motor Controllers
         self.left_motor2 = TalonMotorController(self.left_talon2)
         self.left_motor3 = TalonMotorController(self.left_talon3)
 
         self.right_motor2 = TalonMotorController(self.right_talon2)
         self.right_motor3 = TalonMotorController(self.right_talon3)
 
-
-        # Set the motors to brake when they are not getting signal
+        # Set motors to brake when not receiving signal
         self.left_motor1.setIdleMode(CANSparkMax.IdleMode.kBrake)
-        #self.left_motor2.setNeutralMode(TalonSRX.NeutralMode.Brake)
-        #self.left_motor3.setNeutralMode(TalonSRX.NeutralMode.Brake)
-
         self.right_motor1.setIdleMode(CANSparkMax.IdleMode.kBrake)
-        #self.right_motor2.setNeutralMode(TalonSRX.NeutralMode.Brake)
-        #self.right_motor3.setNeutralMode(TalonSRX.NeutralMode.Brake)
 
-        # Set a current limit on the motors
+        # Set current limit on motors
         self.left_motor1.setSmartCurrentLimit(kCurrentLimit)
         self.right_motor1.setSmartCurrentLimit(kCurrentLimit)
 
-        # Set the motors to reverse so that positive voltages move the robot forward
+        # Set motors to reverse so that positive voltages move the robot forward
         self.left_motor1.setInverted(True)
         self.left_motor2.setInverted(False)
         self.left_motor3.setInverted(False)
@@ -103,25 +95,24 @@ class DifferentalDrive(commands2.Subsystem):
         self.right_motor2.setInverted(True)
         self.right_motor3.setInverted(True)
 
-        # get the spark max encoders
+        # Get spark max encoders
         self.leftEncoder = self.left_motor1.getEncoder()
         self.rightEncoder = self.right_motor1.getEncoder()
 
-        # The motors on the left side of the drive.
+        # Group motors
         self.leftMotors = wpilib.MotorControllerGroup(
             self.left_motor1,
             self.left_motor2,
             self.left_motor3
         )
 
-        # The motors on the right side of the drive.
         self.rightMotors = wpilib.MotorControllerGroup(
             self.right_motor1,
             self.right_motor2,
             self.right_motor3
         )
 
-        # The robot's drive
+        # Initialize drive
         self.drive = wpilib.drive.DifferentialDrive(self.leftMotors, self.rightMotors)
 
     def periodic(self):
@@ -129,11 +120,11 @@ class DifferentalDrive(commands2.Subsystem):
         every 50 calls print out the encoder values
         '''
         self.frame_id += 1
-        #if self.frame_id % 50 == 0:
-            #print("System Time:",time.time())
-            #print("Left Encoder: ", self.leftEncoder.getPosition())
-            #print("Right Encoder: ", self.rightEncoder.getPosition())
-            #print("done")
+        if self.frame_id % 50 == 0:
+            print("System Time:", time.time())
+            print("Left Encoder: ", self.leftEncoder.getPosition())
+            print("Right Encoder: ", self.rightEncoder.getPosition())
+            print("done")
 
     def arcadeDrive(self, fwd: float, rot: float):
         """
@@ -148,7 +139,7 @@ class DifferentalDrive(commands2.Subsystem):
         """
         Stops the drive from moving.
         """
-        self.arcadeDrive(0,0)
+        self.arcadeDrive(0, 0)
 
     def resetEncoders(self):
         """Resets the drive encoders to currently read a position of 0."""
